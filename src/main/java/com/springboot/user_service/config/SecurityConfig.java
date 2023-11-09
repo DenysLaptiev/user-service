2af1ca142f169ea3b465d56ce30702cb169a3c02
DenysLaptiev <laptiev.denys.18@gmail.com>
2023-11-09 16:41:10 +0200

    used default port, refactor SecurityConfig

--- src/main/java/com/springboot/user_service/config/SecurityConfig.java
+++ src/main/java/com/springboot/user_service/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -34,7 +35,7 @@ public class SecurityConfig {
     }
      */
 
-
+/*
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // configure http security
@@ -48,6 +49,25 @@ public class SecurityConfig {
 
         http.oauth2ResourceServer(Customizer.withDefaults());
         http.oauth2Login(Customizer.withDefaults()); // incoming requests should contain jwt token
+
+        return http.build();
+    }
+
+ */
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        // configure http security
+        http
+                .authorizeHttpRequests(authz -> authz
+                        //.requestMatchers(HttpMethod.GET,"/","/home").permitAll()
+                        .anyRequest().authenticated()) // allow only authenticated requests
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                }));
+
+        http.oauth2Login(Customizer.withDefaults()); // incoming requests should contain jwt token
+
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
