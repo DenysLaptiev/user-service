5e2581223b582e94512a5b6030bb48b662fef254
DenysLaptiev <laptiev.denys.18@gmail.com>
2023-11-10 17:48:59 +0200

    added sns

--- src/main/java/com/springboot/user_service/config/SecurityConfig.java
+++ src/main/java/com/springboot/user_service/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot.user_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,16 +18,25 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
         // configure http security
-        http
-                .authorizeHttpRequests(authz -> authz
-                        //.requestMatchers(HttpMethod.GET,"/","/home").permitAll()
-                        .anyRequest().authenticated()) // allow only authenticated requests
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
-                }));
+//        http
+//                .authorizeHttpRequests(authz -> authz
+//                        .requestMatchers(HttpMethod.GET,"/","/home").permitAll()
+//                        .anyRequest().authenticated()) // allow only authenticated requests
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+//                }));
 
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/home").permitAll()
+                .antMatchers("/image-topic").permitAll()
+                .antMatchers("/image-topic/**").permitAll()
+                .anyRequest().authenticated();
         http.oauth2Login(Customizer.withDefaults()); // incoming requests should contain jwt token
 
-        http.csrf(AbstractHttpConfigurer::disable);
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+        }));
 
         return http.build();
     }
