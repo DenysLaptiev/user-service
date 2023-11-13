a74330675d33b74f344e302d7ae267b919dd5a8a
DenysLaptiev <laptiev.denys.18@gmail.com>
2023-11-13 14:16:31 +0200

    commented .antMatchers("/image-to-user").permitAll()

--- src/main/java/com/springboot/user_service/config/security/SecurityConfig.java
+++ src/main/java/com/springboot/user_service/config/security/SecurityConfig.java
@@ -24,8 +24,8 @@ public class SecurityConfig {
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/home").permitAll()
-                .antMatchers("/image-to-user").permitAll()
-                .antMatchers("/image-to-user/**").permitAll()
+//                .antMatchers("/image-to-user").permitAll()
+//                .antMatchers("/image-to-user/**").permitAll()
                 .anyRequest().authenticated();
         http.oauth2Login(Customizer.withDefaults()); // incoming requests should contain jwt token
 
